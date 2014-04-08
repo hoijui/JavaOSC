9ca7ea0df22f852be8b6a460c32885b23dceb98c
hoijui <hoijui.quaero@gmail.com>
2014-04-08 08:48:34 +0200

    OSCPortIn: add an XXX comment [minor]

--- modules/core/src/main/java/com/illposed/osc/OSCPortIn.java
+++ modules/core/src/main/java/com/illposed/osc/OSCPortIn.java
@@ -110,7 +110,7 @@ public class OSCPortIn extends OSCPort implements Runnable {
 						packet.getLength());
 				dispatcher.dispatchPacket(oscPacket);
 			} catch (IOException e) {
-				e.printStackTrace();
+				e.printStackTrace(); // XXX This may not be a good idea, as this could easily lead to a never ending series of exceptions thrown (due to the non-exited while loop), and because the user of the lib may want to handle this case himself
 			}
 		}
 	}
