aa3fd5c985b6785cd1323e4000ebf1b3dec206ee
hoijui <hoijui.quaero@gmail.com>
2014-12-13 15:01:07 +0100

    introduce SizeTrackingOutputStream

---
+++ modules/core/src/main/java/com/illposed/osc/utility/SizeTrackingOutputStream.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014, C. Ramakrishnan / Auracle.
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+package com.illposed.osc.utility;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Keeps track of the number of bytes already written to the stream.
+ */
+public class SizeTrackingOutputStream extends OutputStream {
+
+	private final OutputStream wrappedStream;
+	private int writtenBytes;
+
+	public SizeTrackingOutputStream(final OutputStream wrappedStream) {
+
+		this.wrappedStream = wrappedStream;
+		this.writtenBytes = 0;
+	}
+
+	public int size() {
+		return writtenBytes;
+	}
+
+	public void reset() {
+		writtenBytes = 0;
+	}
+
+	@Override
+	public void write(final int bty) throws IOException {
+		wrappedStream.write(bty);
+		writtenBytes++;
+	}
+
+	@Override
+	public void write(final byte[] bytes) throws IOException {
+		wrappedStream.write(bytes);
+		writtenBytes += bytes.length;
+	}
+
+	@Override
+	public void write(final byte[] bytes, final int off, final int len) throws IOException {
+		wrappedStream.write(bytes, off, len);
+		writtenBytes += len - off;
+	}
+}
