9a14161efadfc88f50454df6819ff93c4a93b02d
hoijui <hoijui.quaero@gmail.com>
2014-12-13 16:47:05 +0100

    OSCPortOut: allow for more control over the charset

--- modules/core/src/main/java/com/illposed/osc/OSCPortOut.java
+++ modules/core/src/main/java/com/illposed/osc/OSCPortOut.java
@@ -89,6 +89,10 @@ public class OSCPortOut extends OSCPort {
 		this(InetAddress.getLocalHost(), DEFAULT_SC_OSC_PORT);
 	}
 
+	public OSCJavaToByteArrayConverter getEncoder() {
+		return converter;
+	}
+
 	/**
 	 * Send an OSC packet (message or bundle) to the receiver we are bound to.
 	 * @param aPacket the bundle or message to send
