7beb028b9a7cb45b5532ce92222d01d4f4d03151
hoijui <hoijui.quaero@gmail.com>
2015-02-26 09:32:24 +0100

    introduce MidiMessageArgumentHandler (OSC 1.0 optional argument type 'm')

---
+++ modules/core/src/main/java/com/illposed/osc/argument/handler/MidiMessageArgumentHandler.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015, C. Ramakrishnan / Illposed Software.
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+package com.illposed.osc.argument.handler;
+
+import com.illposed.osc.argument.OSCMidiMessage;
+import com.illposed.osc.argument.ArgumentHandler;
+import com.illposed.osc.utility.OSCParseException;
+import com.illposed.osc.utility.OSCSerializeException;
+import com.illposed.osc.utility.SizeTrackingOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * Parses and serializes an OSC MIDI message type (4 bytes).
+ */
+public class MidiMessageArgumentHandler implements ArgumentHandler<OSCMidiMessage>, Cloneable {
+
+	public static final ArgumentHandler<OSCMidiMessage> INSTANCE = new MidiMessageArgumentHandler();
+
+	/** Allow overriding, but somewhat enforce the ugly singleton. */
+	protected MidiMessageArgumentHandler() {
+		// ctor declared only for setting the access level
+	}
+
+	@Override
+	public char getDefaultIdentifier() {
+		return 'm';
+	}
+
+	@Override
+	public Class<OSCMidiMessage> getJavaClass() {
+		return OSCMidiMessage.class;
+	}
+
+	@Override
+	public void setProperties(final Map<String, Object> properties) {
+		// we make no use of any properties
+	}
+
+	@Override
+	public boolean isMarkerOnly() {
+		return false;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public MidiMessageArgumentHandler clone() throws CloneNotSupportedException {
+		return (MidiMessageArgumentHandler) super.clone();
+	}
+
+	@Override
+	public OSCMidiMessage parse(final ByteBuffer input) throws OSCParseException {
+		return OSCMidiMessage.valueOf(new byte[] {
+				input.get(),
+				input.get(),
+				input.get(),
+				input.get()});
+	}
+
+	@Override
+	public void serialize(final SizeTrackingOutputStream stream, final OSCMidiMessage value)
+			throws OSCSerializeException
+	{
+		try {
+			stream.write(value.toContentArray());
+		} catch (final IOException ex) {
+			throw new OSCSerializeException(ex);
+		}
+	}
+}
