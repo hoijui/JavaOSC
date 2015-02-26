523d902413b417a1dfeff7791cefe6bdcd968e31
hoijui <hoijui.quaero@gmail.com>
2015-02-26 15:05:30 +0100

    OSCPortIn: use `this.` prefix on fields in ctor [nlc]

--- modules/core/src/main/java/com/illposed/osc/transport/udp/OSCPortIn.java
+++ modules/core/src/main/java/com/illposed/osc/transport/udp/OSCPortIn.java
@@ -65,7 +65,7 @@ public class OSCPortIn extends OSCPort implements Runnable {
 		// NOTE We do this, even though it is against the OSC (1.0) specification,
 		//   because this is how it worked in this library until Feb. 2015.,
 		//   and thus users of this library expect this behavour by default.
-		dispatcher.setAlwaysDispatchingImmediatly(true);
+		this.dispatcher.setAlwaysDispatchingImmediatly(true);
 	}
 
 	/**
