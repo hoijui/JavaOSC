2bfeccdf897479f403227734b2ee08cc8c0828a1
hoijui <hoijui.quaero@gmail.com>
2015-02-26 09:25:09 +0100

    introduce ColorArgumentHandler (OSC 1.0 optional argument type 'r')

---
+++ modules/core/src/main/java/com/illposed/osc/argument/handler/ColorArgumentHandler.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2015, C. Ramakrishnan / Illposed Software.
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+package com.illposed.osc.argument.handler;
+
+import com.illposed.osc.argument.ArgumentHandler;
+import com.illposed.osc.utility.OSCParseException;
+import com.illposed.osc.utility.OSCSerializeException;
+import com.illposed.osc.utility.SizeTrackingOutputStream;
+import java.awt.Color;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * Parses and serializes an OSC RGBA color type (32bit).
+ */
+public class ColorArgumentHandler implements ArgumentHandler<Color>, Cloneable {
+
+	public static final ArgumentHandler<Color> INSTANCE = new ColorArgumentHandler();
+
+	/** Allow overriding, but somewhat enforce the ugly singleton. */
+	protected ColorArgumentHandler() {
+		// ctor declared only for setting the access level
+	}
+
+	@Override
+	public char getDefaultIdentifier() {
+		return 'r';
+	}
+
+	@Override
+	public Class<Color> getJavaClass() {
+		return Color.class;
+	}
+
+	@Override
+	public void setProperties(final Map<String, Object> properties) {
+		// we make no use of any properties
+	}
+
+	@Override
+	public boolean isMarkerOnly() {
+		return false;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public ColorArgumentHandler clone() throws CloneNotSupportedException {
+		return (ColorArgumentHandler) super.clone();
+	}
+
+	/**
+	 * Converts the argument to an {@code int} by an unsigned conversion.
+	 *
+	 * @param signedByte the value to convert to an unsigned {@code int}
+	 * @return the argument converted to {@code int} by an unsigned conversion NOTE Since Java 8,
+	 * one could use Byte#toUnsignedInt
+	 */
+	public static int toUnsignedInt(final byte signedByte) {
+		return ((int) signedByte) & 0xff;
+	}
+
+	/**
+	 * Converts the argument to an {@code byte} by a sign introducing conversion.
+	 *
+	 * @param unsignedInt the value to convert to a signed {@code byte}; has to be in range [0, 255]
+	 * @return the argument converted to {@code byte} by a sign introducing conversion
+	 */
+	public static byte toSignedByte(final int unsignedInt) {
+		return (byte) unsignedInt;
+	}
+
+	@Override
+	public Color parse(final ByteBuffer input) throws OSCParseException {
+		return new Color(
+				toUnsignedInt(input.get()),
+				toUnsignedInt(input.get()),
+				toUnsignedInt(input.get()),
+				toUnsignedInt(input.get()));
+	}
+
+	@Override
+	public void serialize(final SizeTrackingOutputStream stream, final Color value)
+			throws OSCSerializeException
+	{
+		try {
+			stream.write(toSignedByte(value.getRed()));
+			stream.write(toSignedByte(value.getGreen()));
+			stream.write(toSignedByte(value.getBlue()));
+			stream.write(toSignedByte(value.getAlpha()));
+		} catch (final IOException ex) {
+			throw new OSCSerializeException(ex);
+		}
+	}
+}
---
+++ modules/core/src/test/java/com/illposed/osc/argument/handler/ColorArgumentHandlerTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2015, C. Ramakrishnan / Illposed Software.
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+package com.illposed.osc.argument.handler;
+
+import com.illposed.osc.argument.ArgumentHandler;
+import com.illposed.osc.utility.OSCParseException;
+import com.illposed.osc.utility.OSCSerializeException;
+import com.illposed.osc.utility.SizeTrackingOutputStream;
+import java.awt.Color;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Random;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ColorArgumentHandlerTest {
+
+	private static final Color[] DEFAULT_COLORS = new Color[] {
+		Color.BLACK,
+		Color.BLUE,
+		Color.CYAN,
+		Color.DARK_GRAY,
+		Color.GRAY,
+		Color.GREEN,
+		Color.LIGHT_GRAY,
+		Color.MAGENTA,
+		Color.ORANGE,
+		Color.PINK,
+		Color.RED,
+		Color.WHITE,
+		Color.YELLOW};
+
+	public static <T> T reparse(final ArgumentHandler<T> type, final int bufferSize, final T orig)
+			throws OSCSerializeException, OSCParseException
+	{
+		// serialize
+		final ByteArrayOutputStream buffer = new ByteArrayOutputStream(bufferSize);
+		final SizeTrackingOutputStream trackedBuffer = new SizeTrackingOutputStream(buffer);
+		type.serialize(trackedBuffer, orig);
+		final ByteBuffer reparsableBuffer
+				= ByteBuffer.wrap(buffer.toByteArray()).asReadOnlyBuffer();
+
+		// re-parse
+		final T reparsed = type.parse(reparsableBuffer);
+
+		return reparsed;
+	}
+
+	private static Color reparse(final Color orig) throws OSCSerializeException, OSCParseException {
+		return reparse(ColorArgumentHandler.INSTANCE, 4, orig);
+	}
+
+	@Test
+	public void testReconvertBytes() {
+
+		final byte[] testBytes = new byte[] {
+			-128,
+			-1,
+			0,
+			1,
+			127
+		};
+		final int[] testInts = new int[] {
+			128,
+			255,
+			0,
+			1,
+			127
+		};
+
+		for (int tni = 0; tni < testBytes.length; tni++) {
+			final byte origByte = testBytes[tni];
+			final int origInt = testInts[tni];
+
+			final int createdInt = ColorArgumentHandler.toUnsignedInt(origByte);
+			Assert.assertEquals(origInt, createdInt);
+
+			final byte createdByte = ColorArgumentHandler.toSignedByte(createdInt);
+			Assert.assertEquals(origByte, createdByte);
+		}
+	}
+
+	@Test
+	public void testReparseDefaultColors() throws OSCSerializeException, OSCParseException {
+
+		for (final Color orig : DEFAULT_COLORS) {
+			Assert.assertEquals(orig, reparse(orig));
+		}
+	}
+
+	/**
+	 * Adds random alpha values between 0 and 255 to the default colors,
+	 * and then tries to re-parse them.
+	 * @throws OSCSerializeException on re-parse failure
+	 * @throws OSCParseException on re-parse failure
+	 */
+	@Test
+	public void testReparseDefaultColorsAlphaed() throws OSCSerializeException, OSCParseException {
+
+		final long alphaRandomSeed = new Random().nextLong();
+		System.out.println(ColorArgumentHandlerTest.class.getSimpleName()
+				+ "#testReparseDefaultColorsAlphaed:alphaRandomSeed: " + alphaRandomSeed);
+		final Random alphaRandom = new Random(alphaRandomSeed);
+		final Color[] alphaedDefaultColors = Arrays.copyOf(DEFAULT_COLORS, DEFAULT_COLORS.length);
+		for (int tci = 0; tci < alphaedDefaultColors.length; tci++) {
+			final Color orig = alphaedDefaultColors[tci];
+			final int alpha = alphaRandom.nextInt(256);
+			final Color alphaed = new Color(orig.getRed(), orig.getGreen(), orig.getBlue(), alpha);
+			alphaedDefaultColors[tci] = alphaed;
+		}
+
+		for (final Color origColor : alphaedDefaultColors) {
+			Assert.assertEquals(origColor, reparse(origColor));
+		}
+	}
+}
