532a068145170e86650942321cbd1c4c5fd81f75
hoijui <hoijui.quaero@gmail.com>
2015-02-26 09:32:24 +0100

    introduce MidiMessageArgumentHandler (OSC 1.0 optional argument type 'm')

---
+++ modules/core/src/test/java/com/illposed/osc/argument/handler/MidiMessageArgumentHandlerTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2015, C. Ramakrishnan / Illposed Software.
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+package com.illposed.osc.argument.handler;
+
+import com.illposed.osc.argument.OSCMidiMessage;
+import com.illposed.osc.utility.OSCParseException;
+import com.illposed.osc.utility.OSCSerializeException;
+import java.util.Random;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MidiMessageArgumentHandlerTest {
+
+	private static OSCMidiMessage reparse(final OSCMidiMessage orig)
+			throws OSCSerializeException, OSCParseException
+	{
+		return ColorArgumentHandlerTest.reparse(
+				MidiMessageArgumentHandler.INSTANCE,
+				OSCMidiMessage.NUM_CONTENT_BYTES,
+				orig);
+	}
+
+	private static void testReparse(final OSCMidiMessage orig)
+			throws OSCSerializeException, OSCParseException
+	{
+		Assert.assertEquals(orig, reparse(orig));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testReparseNull() throws OSCSerializeException, OSCParseException {
+
+		final OSCMidiMessage orig = null;
+		Assert.assertEquals(orig, reparse(orig));
+	}
+
+	private static void testContent(final byte[] content) {
+		OSCMidiMessage.valueOf(content);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testContentBytesZero() {
+		testContent(new byte[0]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testContentBytesOne() {
+		testContent(new byte[1]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testContentBytesTwo() {
+		testContent(new byte[2]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testContentBytesThree() {
+		testContent(new byte[3]);
+	}
+
+	@Test
+	public void testContentBytesfour() {
+		testContent(new byte[4]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testContentBytesFive() {
+		testContent(new byte[5]);
+	}
+
+	@Test
+	public void testReparseZeros() throws OSCSerializeException, OSCParseException {
+		testReparse(OSCMidiMessage.valueOf(new byte[] {0, 0, 0, 0}));
+	}
+
+	@Test
+	public void testReparsePositive() throws OSCSerializeException, OSCParseException {
+		testReparse(OSCMidiMessage.valueOf(new byte[] {127, 127, 127, 127}));
+	}
+
+	@Test
+	public void testReparseNegative() throws OSCSerializeException, OSCParseException {
+		testReparse(OSCMidiMessage.valueOf(new byte[] {-128, -128, -128, -128}));
+	}
+
+	@Test
+	public void testReparseRandom() throws OSCSerializeException, OSCParseException {
+
+		final long contentRandomSeed = new Random().nextLong();
+		System.out.println(MidiMessageArgumentHandlerTest.class.getSimpleName()
+				+ "#testReparseRandom:contentRandomSeed: " + contentRandomSeed);
+		final Random contentRandom = new Random(contentRandomSeed);
+		final byte[] content = new byte[4];
+		contentRandom.nextBytes(content);
+		testReparse(OSCMidiMessage.valueOf(content));
+	}
+}
