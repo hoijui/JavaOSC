f7856c42faf24e1a70335c0b90ce91e1bd07b47d
hoijui <hoijui.quaero@gmail.com>
2015-03-03 08:22:32 +0100

    StringArgumentHandler: improve a comment [minor]

--- modules/core/src/main/java/com/illposed/osc/argument/handler/StringArgumentHandler.java
+++ modules/core/src/main/java/com/illposed/osc/argument/handler/StringArgumentHandler.java
@@ -108,7 +108,7 @@ public class StringArgumentHandler implements ArgumentHandler<String>, Cloneable
 		input.position(input.position() + strLen);
 		// because strings are always padded with at least one zero,
 		// as their length is not given in advance, as is the case with blobs
-		input.get(); // position++
+		input.get(); // position++ to skip the terminating {@code (byte) 0}
 		OSCParser.align(input);
 		return res;
 	}
