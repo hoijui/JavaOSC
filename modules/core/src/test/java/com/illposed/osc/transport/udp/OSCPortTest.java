8968d6eb2f77fc66b78a3bec1206cc5d80c93780
hoijui <hoijui.quaero@gmail.com>
2015-12-28 14:34:20 +0100

    add bundle sending and receiving tests [test]

--- modules/core/src/test/java/com/illposed/osc/transport/udp/OSCPortTest.java
+++ modules/core/src/test/java/com/illposed/osc/transport/udp/OSCPortTest.java
@@ -11,9 +11,14 @@ package com.illposed.osc.transport.udp;
 import com.illposed.osc.OSCBundle;
 import com.illposed.osc.OSCMessage;
 import com.illposed.osc.OSCPacket;
+import com.illposed.osc.OSCParserFactory;
+import com.illposed.osc.OSCSerializerFactory;
 import com.illposed.osc.SimpleOSCMessageListener;
 import com.illposed.osc.messageselector.OSCPatternAddressMessageSelector;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.DatagramChannel;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
@@ -33,10 +38,66 @@ public class OSCPortTest {
 	private OSCPortOut sender;
 	private OSCPortIn  receiver;
 
+	private void reSetUp(
+			final int portSenderOut,
+			final int portSenderIn,
+			final int portReceiverOut,
+			final int portReceiverIn)
+			throws Exception
+	{
+		final SocketAddress senderOutAddress = new InetSocketAddress(portSenderOut);
+		final SocketAddress senderInAddress = new InetSocketAddress(portSenderIn);
+		final SocketAddress receiverOutAddress = new InetSocketAddress(portReceiverOut);
+		final SocketAddress receiverInAddress = new InetSocketAddress(portReceiverIn);
+
+		if (receiver != null) {
+			receiver.close();
+		}
+		receiver = new OSCPortIn(
+				OSCParserFactory.createDefaultFactory(),
+				receiverInAddress,
+				senderInAddress);
+
+		if (sender != null) {
+			sender.close();
+		}
+		sender = new OSCPortOut(
+				OSCSerializerFactory.createDefaultFactory(),
+				receiverOutAddress,
+				senderOutAddress);
+	}
+
+	private void reSetUp(final int portSender, final int portReceiver) throws Exception {
+		reSetUp(portSender, portSender, portReceiver, portReceiver);
+	}
+
+	private void reSetUp(final int portReceiver) throws Exception {
+		reSetUp(0, portReceiver);
+	}
+
 	@Before
 	public void setUp() throws Exception {
-		sender = new OSCPortOut();
-		receiver = new OSCPortIn(OSCPort.defaultSCOSCPort());
+		reSetUp(OSCPort.defaultSCOSCPort());
+	}
+
+	private void reSetUpDifferentPorts() throws Exception {
+		reSetUp(OSCPort.defaultSCOSCPort(), OSCPort.defaultSCOSCPort() + 1);
+	}
+
+	private void reSetUpDifferentSender() throws Exception {
+		reSetUp(
+				OSCPort.defaultSCOSCPort(),
+				OSCPort.defaultSCOSCPort() + 1,
+				OSCPort.defaultSCOSCPort() + 2,
+				OSCPort.defaultSCOSCPort() + 2);
+	}
+
+	private void reSetUpDifferentReceiver() throws Exception {
+		reSetUp(
+				OSCPort.defaultSCOSCPort(),
+				OSCPort.defaultSCOSCPort(),
+				OSCPort.defaultSCOSCPort() + 1,
+				OSCPort.defaultSCOSCPort() + 2);
 	}
 
 	@After
@@ -99,18 +160,21 @@ public class OSCPortTest {
 		Assert.assertEquals("Bad default SuperCollider Language OSC port",
 				57120, OSCPort.defaultSCLangOSCPort());
 
+		InetSocketAddress remoteAddress = ((InetSocketAddress) sender.getRemoteAddress());
 		Assert.assertEquals("Bad default port with ctor()",
-				57110, sender.getPort());
+				57110, remoteAddress.getPort());
 
 		sender.close();
 		sender = new OSCPortOut(InetAddress.getLocalHost());
+		remoteAddress = ((InetSocketAddress) sender.getRemoteAddress());
 		Assert.assertEquals("Bad default port with ctor(address)",
-				57110, sender.getPort());
+				57110, remoteAddress.getPort());
 
 		sender.close();
 		sender = new OSCPortOut(InetAddress.getLocalHost(), 12345);
+		remoteAddress = ((InetSocketAddress) sender.getRemoteAddress());
 		Assert.assertEquals("Bad port with ctor(address, port)",
-				12345, sender.getPort());
+				12345, remoteAddress.getPort());
 	}
 
 	@Test
@@ -189,5 +253,84 @@ public class OSCPortTest {
 			Assert.fail("Message should have timestamp " + bundle.getTimestamp()
 					+ " but has " + listener.getReceivedTimestamp());
 		}
+	}
+
+	private void testBundleReceiving(final boolean shouldReceive) throws Exception {
+
+		OSCBundle bundle = new OSCBundle();
+		bundle.addPacket(new OSCMessage("/bundle/receiving"));
+		SimpleOSCMessageListener listener = new SimpleOSCMessageListener();
+		receiver.getDispatcher().addListener(new OSCPatternAddressMessageSelector("/bundle/receiving"),
+				listener);
+		receiver.startListening();
+
+		sender.send(bundle);
+		Thread.sleep(100); // wait a bit
+		receiver.stopListening();
+
+//		receiver.disconnect();
+
+		if (shouldReceive && !listener.isMessageReceived()) {
+			Assert.fail("Message was not received");
+		} else if (!shouldReceive && listener.isMessageReceived()) {
+			Assert.fail("Message was received while it should not have!");
+		}
+	}
+
+	@Test
+	public void testBundleReceivingConnectedOut() throws Exception {
+//		reSetUpDifferentPorts();
+
+		sender.connect();
+		testBundleReceiving(true);
+	}
+
+	@Test
+	public void testBundleReceivingConnectedOutDifferentSender() throws Exception {
+		reSetUpDifferentSender();
+
+		sender.connect();
+		testBundleReceiving(true);
+	}
+
+	@Test
+	public void testBundleReceivingConnectedOutDifferentReceiver() throws Exception {
+		reSetUpDifferentReceiver();
+
+		sender.connect();
+		testBundleReceiving(false);
+	}
+
+	@Test
+	public void testBundleReceivingConnectedIn() throws Exception {
+		reSetUpDifferentPorts();
+
+		receiver.connect();
+		testBundleReceiving(true);
+	}
+
+	@Test
+	public void testBundleReceivingConnectedInDifferentSender() throws Exception {
+		reSetUpDifferentSender();
+
+		receiver.connect();
+		testBundleReceiving(false);
+	}
+
+	@Test
+	public void testBundleReceivingConnectedInDifferentReceiver() throws Exception {
+		reSetUpDifferentReceiver();
+
+		receiver.connect();
+		testBundleReceiving(false);
+	}
+
+	@Test
+	public void testBundleReceivingConnectedBoth() throws Exception {
+		reSetUpDifferentPorts();
+
+		receiver.connect();
+		sender.connect();
+		testBundleReceiving(true);
 	}
 }
