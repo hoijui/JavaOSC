97df5907d3bad903799a17532ca782d4d6bfe547
hoijui <hoijui.quaero@gmail.com>
2016-05-20 17:45:33 +0200

    always stop listening before closing [fix]

--- modules/core/src/main/java/com/illposed/osc/transport/udp/OSCPortIn.java
+++ modules/core/src/main/java/com/illposed/osc/transport/udp/OSCPortIn.java
@@ -190,6 +190,15 @@ public class OSCPortIn extends OSCPort implements Runnable {
 	}
 
 	@Override
+	public void close() throws IOException {
+
+		if (isListening()) {
+			stopListening();
+		}
+		super.close();
+	}
+
+	@Override
 	public String toString() {
 
 		final StringBuilder rep = new StringBuilder(32);
