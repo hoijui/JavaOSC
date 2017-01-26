42443a52119c1118bce1b6d7480becf42c686f2b
hoijui <hoijui.quaero@gmail.com>
2017-01-26 08:57:32 +0100

    cleanup OSCDatagramChannel

--- modules/core/src/main/java/com/illposed/osc/transport/channel/OSCDatagramChannel.java
+++ modules/core/src/main/java/com/illposed/osc/transport/channel/OSCDatagramChannel.java
@@ -27,43 +27,33 @@ import java.nio.channels.Selector;
 import java.nio.channels.spi.SelectorProvider;
 
 /**
- * TODO.
- * An abstract superclass.
+ * This class handles just the basic sending and receiving of OSC data
+ * over a DatagramChannel.
+ * It is mostly for internal use,
+ * and will probably not be used directly by users of this library.
  * To send OSC messages, use {@link OSCPortOut}.
  * To listen for OSC messages, use {@link OSCPortIn}.
  */
 public class OSCDatagramChannel extends SelectableChannel {
 
 	private final DatagramChannel underlyingChannel;
-//	private final ByteBuffer buffer;
 	private final OSCParser parser;
-//	private final OSCSerializer serializer;
 	private final OSCSerializerFactory serializerFactory;
 
 	public <C extends ByteChannel, InterruptibleChannel> OSCDatagramChannel(
 			final DatagramChannel underlyingChannel,
 			final OSCParserFactory parserFactory,
-			final OSCSerializerFactory serializerFactory/*,
-			final ByteBuffer buffer*/)
+			final OSCSerializerFactory serializerFactory
+			)
 	{
-//		super(underlyingChannel.provider());
-
 		this.underlyingChannel = underlyingChannel;
-//		this.buffer = buffer;
 		OSCParser tmpParser = null;
-//		this.parser = (parserFactory == null) ? null : parserFactory.create();
 		if (parserFactory != null) {
 			tmpParser = parserFactory.create();
 		}
 		this.parser = tmpParser;
-//		this.serializer = serializerFactory.create();
 		this.serializerFactory = serializerFactory;
 	}
-
-//	public static <C extends ByteChannel, InterruptibleChannel> OSCDatagramChannel open(final C underlyingChannel) throws IOException {
-//
-//		return new OSCDatagramChannel(underlyingChannel);
-//	}
 
 	public OSCPacket read(final ByteBuffer buffer) throws IOException, OSCParseException {
 
@@ -72,64 +62,28 @@ public class OSCDatagramChannel extends SelectableChannel {
 		try {
 			begin();
 
-
-
-//			try {
-				buffer.clear();
-//				try {
-//System.err.println("before receive/read ...");
-					if (underlyingChannel.isConnected()) {
-						/*final int readBytes = */underlyingChannel.read(buffer);
-					} else {
-						underlyingChannel.receive(buffer);
-					}
-//System.err.println("after receive/read.");
-//					final int readBytes = buffer.position();
-//				} catch (final ClosedChannelException ex) {
-//					if (listening) {
-//						throw ex;
-//					} else {
-//						// if we closed the channel while receiving data,
-//						// the exception is expected/normal, so we hide it
-//						continue;
-//					}
-//				}
-				buffer.flip();
-				if (buffer.limit() == 0) {
-//					if (isListening()) {
-						throw new OSCParseException("Received a packet without any data");
-//					} else {
-//						// normal exit: we just get a no-data package becasue we stopped listening
-//						break;
-//					}
-				} else {
-					// convert from OSC byte array -> Java object
-					// FIXME BAAAAAAAD!! - the overflow would happen on the receiving end, up there, not down here!
-//					do {
-//						try {
-							oscPacket = parser.convert(buffer);
-//						} catch (final BufferOverflowException ex) {
-//							buffer = ByteBuffer.allocate(buffer.capacity() + BUFFER_SIZE);
-//						}
-//					} while (oscPacket == null);
-
-					// dispatch the Java object
-//					dispatcher.dispatchPacket(oscPacket);
-					completed = true;
-				}
-//			} catch (final IOException ex) {
-//				stopListening(ex);
-//			} catch (final OSCParseException ex) {
-//				throw new IOException(ex);
+			buffer.clear();
+			// NOTE From the doc of `read()` and `receive()`:
+			// "If there are fewer bytes remaining in the buffer
+			// than are required to hold the datagram
+			// then the remainder of the datagram is silently discarded."
+			if (underlyingChannel.isConnected()) {
+				underlyingChannel.read(buffer);
+			} else {
+				underlyingChannel.receive(buffer);
+			}
+//			final int readBytes = buffer.position();
+//			if (readBytes == buffer.capacity()) {
+//				// TODO In this case it is very likely that the buffer was actually too small, and the remainder of the datagram/packet was silently discarded. We might want to give a warning, like throw an exception in this case, but whether this happens should probably be user configurable.
 //			}
-
-
-//			completed = vvv;    // Perform blocking I/O operation
-//			return xxx;         // Return result
-//		} catch (final Exception ex) {
-//ex.printStackTrace();
+			buffer.flip();
+			if (buffer.limit() == 0) {
+				throw new OSCParseException("Received a packet without any data");
+			} else {
+				oscPacket = parser.convert(buffer);
+				completed = true;
+			}
 		} finally {
-//if (!completed) { System.err.println("!completed receive/read."); }
 			end(completed);
 		}
 
@@ -174,105 +128,6 @@ public class OSCDatagramChannel extends SelectableChannel {
 		}
 	}
 
-
-//	@Override
-//	protected void implCloseChannel() throws IOException {
-//		throw new UnsupportedOperationException("Not supported yet.");
-//	}
-
-//	@Override
-//	protected void implCloseSelectableChannel() throws IOException {
-//		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//	}
-//
-//	@Override
-//	protected void implConfigureBlocking(boolean block) throws IOException {
-//		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//	}
-//
-//	@Override
-//	public int validOps() {
-//		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//	}
-
-//	private final DatagramChannel channel;
-//
-//	public static final int DEFAULT_SC_OSC_PORT = 57110;
-//	public static final int DEFAULT_SC_LANG_OSC_PORT = 57120;
-//
-//	protected OSCDatagramChannel(final SocketAddress local, final SocketAddress remote) throws IOException {
-//
-//		this.local = local;
-//		this.remote = remote;
-//		this.channel = DatagramChannel.open();
-//
-//		this.channel.setOption(java.net.StandardSocketOptions.SO_REUSEADDR, true); // XXX This is only available since Java 1.7
-//		this.channel.socket().bind(local);
-//	}
-//
-//	/**
-//	 * The port that the SuperCollider <b>synth</b> engine
-//	 * usually listens to.
-//	 * @return default SuperCollider <b>synth</b> UDP port
-//	 * @see #DEFAULT_SC_OSC_PORT
-//	 */
-//	public static int defaultSCOSCPort() {
-//		return DEFAULT_SC_OSC_PORT;
-//	}
-//
-//	/**
-//	 * The port that the SuperCollider <b>language</b> engine
-//	 * usually listens to.
-//	 * @return default SuperCollider <b>language</b> UDP port
-//	 * @see #DEFAULT_SC_LANG_OSC_PORT
-//	 */
-//	public static int defaultSCLangOSCPort() {
-//		return DEFAULT_SC_LANG_OSC_PORT;
-//	}
-//
-//	/**
-//	 * Returns the channel associated with this port.
-//	 * @return this ports channel
-//	 */
-//	protected DatagramChannel getChannel() {
-//		return channel;
-//	}
-//
-//	public void connect() throws IOException {
-//
-//		if (getRemoteAddress() == null) {
-//			throw new IllegalStateException(
-//					"Can not connect a socket without a remote address specified");
-//		}
-//		getChannel().connect(getRemoteAddress());
-//	}
-//
-//	public void disconnect() throws IOException {
-//		getChannel().disconnect();
-//	}
-//
-//	public boolean isConnected() {
-//		return getChannel().isConnected();
-//	}
-//
-//	public SocketAddress getLocalAddress() {
-//		return local;
-//	}
-//
-//	public SocketAddress getRemoteAddress() {
-//		return remote;
-//	}
-//
-//	/**
-//	 * Close the socket and free-up resources.
-//	 * It is recommended that clients call this when they are done with the
-//	 * port.
-//	 * @throws IOException If an I/O error occurs on the channel
-//	 */
-//	public void close() throws IOException {
-//		channel.close();
-//	}
-
 	@Override
 	public SelectorProvider provider() {
 		return underlyingChannel.provider();
@@ -310,7 +165,8 @@ public class OSCDatagramChannel extends SelectableChannel {
 
 	@Override
 	protected void implCloseChannel() throws IOException {
-		underlyingChannel.close(); // XXX is this ok?
+		// XXX is this ok?
+		underlyingChannel.close();
 	}
 
 	@Override
