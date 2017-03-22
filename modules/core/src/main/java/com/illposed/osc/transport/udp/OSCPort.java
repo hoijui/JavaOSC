f019dea440d8a34d08a2c64ff05af99d6aaedb78
hoijui <hoijui.quaero@gmail.com>
2017-03-22 07:35:38 +0100

    allow to re-use listening socket

--- modules/core/src/main/java/com/illposed/osc/transport/udp/OSCPort.java
+++ modules/core/src/main/java/com/illposed/osc/transport/udp/OSCPort.java
@@ -32,6 +32,8 @@ public class OSCPort {
 		this.remote = remote;
 		this.channel = DatagramChannel.open();
 
+		// NOTE StandardSocketOptions is only available since Java 1.7
+		this.channel.setOption(java.net.StandardSocketOptions.SO_REUSEADDR, true);
 		this.channel.socket().bind(local);
 	}
 
