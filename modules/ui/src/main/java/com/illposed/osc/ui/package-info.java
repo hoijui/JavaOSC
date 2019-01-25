3b1b67f7c8db2c4659f153a6ce326eea8018aa0b
hoijui <hoijui.quaero@gmail.com>
2019-01-25 12:20:12 +0100

    UI: add package-info.java [nlc]

---
+++ modules/ui/src/main/java/com/illposed/osc/ui/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2019, C. Ramakrishnan / Illposed Software.
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+/**
+ * Provides a simple test GUI to send SuperCollider OSC messages over the UDP(/IP) protocol.
+ */
+package com.illposed.osc.ui;
