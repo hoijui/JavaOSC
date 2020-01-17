fb8562ac2b763928800157db919d27683536b117
Dave Yarwood <dave.yarwood@gmail.com>
2020-01-16 20:49:49 -0500

    DebugClient to test serializing/sending packets

--- modules/core/src/test/java/com/illposed/osc/DebugServer.java
+++ modules/core/src/test/java/com/illposed/osc/DebugServer.java
@@ -10,69 +10,14 @@ package com.illposed.osc;
 //   -Dexec.args="udp 12345" # protocol and port number
 
 import com.illposed.osc.OSCBadDataEvent;
-import com.illposed.osc.OSCBundle;
-import com.illposed.osc.OSCMessage;
-import com.illposed.osc.OSCPacket;
 import com.illposed.osc.OSCPacketEvent;
 import com.illposed.osc.OSCPacketListener;
 import com.illposed.osc.transport.NetworkProtocol;
 import com.illposed.osc.transport.OSCPortIn;
 import com.illposed.osc.transport.OSCPortInBuilder;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 public class DebugServer
 {
-	private static String indent(String str, int indentLevel) {
-		String indentation = String.join("", Collections.nCopies(indentLevel, "  "));
-
-		return Arrays.asList(str.split("\\R"))
-			.stream()
-			.map(line -> indentation + line)
-			.collect(Collectors.joining("\n"));
-	}
-
-	private static String debug(OSCPacket packet, int indentLevel) {
-		if (packet instanceof OSCBundle) {
-			OSCBundle bundle = (OSCBundle)packet;
-
-			StringBuilder sb = new StringBuilder();
-			sb.append(String.format("bundle (%s)", bundle.getTimestamp()));
-
-			for (OSCPacket entry : bundle.getPackets()) {
-				sb.append("\n" + indent(debug(entry), indentLevel + 1));
-			}
-
-			return sb.toString();
-		}
-
-		OSCMessage message = (OSCMessage)packet;
-
-		StringBuilder sb = new StringBuilder();
-		sb.append(String.format("message (%s)", message.getAddress()));
-
-		for (Object argument : message.getArguments()) {
-			sb.append(
-				"\n" +
-				indent(
-					argument == null
-						? "[null]"
-						: String.format(
-							"[%s] %s", argument.getClass(), argument.toString()
-						),
-					indentLevel + 1
-				)
-			);
-		}
-
-		return sb.toString();
-	}
-
-	private static String debug(OSCPacket packet) {
-		return debug(packet, 0);
-	}
-
 	public static void main(String[] args) throws Exception
 	{
 		if (args.length != 2) {
@@ -98,7 +43,7 @@ public class DebugServer
 
 		OSCPacketListener debugger = new OSCPacketListener() {
 			public void handlePacket(OSCPacketEvent event) {
-				System.out.println(debug(event.getPacket()) + "\n");
+				System.out.println(Debug.debug(event.getPacket()) + "\n");
 			}
 
 			public void handleBadData(OSCBadDataEvent event) {
