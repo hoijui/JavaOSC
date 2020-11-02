26d58961158bf9db1df1aa4e75666cacdddffbdc
hoijui <hoijui.quaero@gmail.com>
2020-11-02 10:54:28 +0100

    Use the actual handler, not its test class #56

--- modules/core/src/main/java/com/illposed/osc/argument/handler/Activator.java
+++ modules/core/src/main/java/com/illposed/osc/argument/handler/Activator.java
@@ -88,7 +88,7 @@ public final class Activator {
 		// NOTE As not all JRE's support AWT (for example headless ones and Android)),
 		//      we handle it this way.
 		try {
-			final String awtColorArgHClsName = ColorArgumentHandler.class.getPackage().getName() + ".AwtColorArgumentHandlerTest";
+			final String awtColorArgHClsName = ColorArgumentHandler.class.getPackage().getName() + ".AwtColorArgumentHandler";
 			final Class<?> awtColorArgumentHandler = Activator.class.getClassLoader().loadClass(awtColorArgHClsName);
 			final Field instance = awtColorArgumentHandler.getDeclaredField("INSTANCE");
 			instance.setAccessible(true);
